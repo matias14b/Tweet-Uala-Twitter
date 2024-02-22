b7e9c5add2def40ec8f0dd7d080fa1803991bc60
mbotteri <zfmKryK#GYZn8u$>
2024-02-22 03:16:09 -0300

    -Se agregaron los servicios tweet y usuario -Se crearon los escenarios de test para crear un tweet -Se creo un Rest controller para exponer la creacion de un tweet

---
+++ usuario-service/src/test/java/com/ejercicio/uala/usuario/UsuarioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicio.uala.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
